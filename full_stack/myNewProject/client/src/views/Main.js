719017581a3c1eefbae73c81d494f51f881522c0
williamzhanshum <williamzhan123@gmail.com>
2022-11-12 09:50:33 -0800

    deleted myNewProject folder

--- full_stack/myNewProject/client/src/views/Main.js
+++
@@ -1,7 +0,0 @@
-import React, { useEffect, useState } from 'react';
-import axios from 'axios';
-import PersonForm from '../components/PersonForm';
-
-export default () => {
-  return <PersonForm />;
-};
